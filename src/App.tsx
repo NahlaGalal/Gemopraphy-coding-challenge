00d96d5ddca2e19845b418b54f460fcac11049ce
Nahla Galal <nahlaglal@gmail.com>
2021-02-17 07:03:43 +0200

    Add Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 // Components
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
 const App: React.FC<{}> = () => {
-  return <div className="App">
-    <Navbar />
-  </div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Card
+        userAvatar="https://via.placeholder.com/175"
+        repoName="Tensor flow"
+        repoDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra feugiat sagittis dui interdum. Turpis et risus, faucibus et ultrices mi diam. Volutpat dui vel purus integer ornare pulvinar amet. Purus turpis sagittis, sagittis rhoncus purus nunc feugiat consequat. Tincidunt eu semper nec volutpat morbi. Arcu, donec lorem tortor mauris, nulla. Arcu ridiculus nibh tellus facilisi. Porttitor potenti quam arcu amet, egestas quis odio mi."
+        starCount={2}
+        issuesCount={2}
+        createdAt={new Date()}
+        author="Tensor flow"
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React from "react";
+// Images
+import starIcon from "../images/star.svg";
+import issueIcon from "../images/issue.svg";
 
 interface Props {
-  
+  userAvatar: string;
+  repoName: string;
+  repoDesc: string;
+  starCount: number;
+  issuesCount: number;
+  createdAt: Date;
+  author: string;
 }
 
-const card = (props: Props) => {
+const Card: React.FC<Props> = ({
+  userAvatar,
+  repoName,
+  repoDesc,
+  starCount,
+  issuesCount,
+  createdAt,
+  author,
+}) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="repo-card">
+      <img src={userAvatar} alt="User avatar" />
+      <div className="repo-data">
+        <h2>{repoName}</h2>
+        <p>{repoDesc}</p>
+        <footer>
+          <div className="">
+            <div>
+              <img src={starIcon} alt="Number of stars"/>
+              <span>Star</span>
+            </div>
+            <span>{starCount}</span>
+          </div>
+          <div>
+            <div>
+              <img src={issueIcon} alt="Number of issues"/>
+              <span>Issues</span>
+            </div>
+            <span>{issuesCount}</span>
+          </div>
+          <p>Submitted {createdAt.getHours()} days ago by <span>{author}</span></p>
+        </footer>
+      </div>
+    </section>
+  );
+};
 
-export default card
+export default Card;
