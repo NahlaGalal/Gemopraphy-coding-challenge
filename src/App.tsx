cf8fdea8c3aa4d93b09941d1ae2363cae0d914c5
Nahla Galal <nahlaglal@gmail.com>
2021-02-17 07:28:13 +0200

    Add semantics to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,30 @@ import Card from "./components/Card";
 const App: React.FC<{}> = () => {
   return (
     <div className="App">
-      <Navbar />
-      <Card
-        userAvatar="https://via.placeholder.com/175"
-        repoName="Tensor flow"
-        repoDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra feugiat sagittis dui interdum. Turpis et risus, faucibus et ultrices mi diam. Volutpat dui vel purus integer ornare pulvinar amet. Purus turpis sagittis, sagittis rhoncus purus nunc feugiat consequat. Tincidunt eu semper nec volutpat morbi. Arcu, donec lorem tortor mauris, nulla. Arcu ridiculus nibh tellus facilisi. Porttitor potenti quam arcu amet, egestas quis odio mi."
-        starCount={2}
-        issuesCount={2}
-        createdAt={new Date()}
-        author="Tensor flow"
-      />
+      <header>
+        <Navbar />
+        <h1>Most Starred Repos</h1>
+      </header>
+      <main>
+        <Card
+          userAvatar="https://via.placeholder.com/175"
+          repoName="Tensor flow"
+          repoDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra feugiat sagittis dui interdum. Turpis et risus, faucibus et ultrices mi diam. Volutpat dui vel purus integer ornare pulvinar amet. Purus turpis sagittis, sagittis rhoncus purus nunc feugiat consequat. Tincidunt eu semper nec volutpat morbi. Arcu, donec lorem tortor mauris, nulla. Arcu ridiculus nibh tellus facilisi. Porttitor potenti quam arcu amet, egestas quis odio mi."
+          starCount={2}
+          issuesCount={2}
+          createdAt={new Date()}
+          author="Tensor flow"
+        />
+        <Card
+          userAvatar="https://via.placeholder.com/175"
+          repoName="Tensor flow"
+          repoDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra feugiat sagittis dui interdum. Turpis et risus, faucibus et ultrices mi diam. Volutpat dui vel purus integer ornare pulvinar amet. Purus turpis sagittis, sagittis rhoncus purus nunc feugiat consequat. Tincidunt eu semper nec volutpat morbi. Arcu, donec lorem tortor mauris, nulla. Arcu ridiculus nibh tellus facilisi. Porttitor potenti quam arcu amet, egestas quis odio mi."
+          starCount={2}
+          issuesCount={2}
+          createdAt={new Date()}
+          author="Tensor flow"
+        />
+      </main>
     </div>
   );
 };
